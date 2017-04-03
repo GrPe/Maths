adb43bb300c248993798733eb7afb9ddf61489bc
Grzegorz <grzesiek.p9804@gmail.com>
2017-04-03 22:58:27 +0200

    Added FalseAnswer function in Addition.cs

--- Resources/Activities/Addition2.cs
+++ Resources/Activities/Addition2.cs
@@ -18,6 +18,7 @@ namespace Maths.Resources.Activities
     public class Addition2 : Activity
     {
         BasicMathsStructures.ValueInt2 expression = new BasicMathsStructures.ValueInt2();
+        bool flag = true;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -27,18 +28,16 @@ namespace Maths.Resources.Activities
             TextView mTextView = FindViewById<TextView>(Resource.Id.textview_View_BaseMahs);
             EditText mEditText = FindViewById<EditText>(Resource.Id.edittext_View_BaseMaths);
             Button mbutton = FindViewById<Button>(Resource.Id.button_View_BaseMaths_Next);
-            action(mTextView,mEditText,mbutton);
-            bool flag = true;
+            Action(mTextView,mEditText,mbutton);
             mbutton.Click += delegate
             {
-                if (flag) action(mTextView, mEditText, mbutton);
-                else /*code false answer*/
+                if (flag) Action(mTextView, mEditText, mbutton);
+                else FalseAnswer(mTextView, mEditText);
             };
         }
 
-        private void action(TextView mTextView,EditText mEditText,Button mbutton)
+        private void Action(TextView mTextView,EditText mEditText,Button mbutton)
         {
-
             mbutton.Enabled = false;
             mbutton.SetBackgroundColor(Android.Graphics.Color.ParseColor("#778899")); //gray
             mEditText.Text = "";
@@ -51,18 +50,19 @@ namespace Maths.Resources.Activities
                 {
                     if (mEditText.Text != "")
                     {
-                        if (BasicMathsFunctions.VerifyAddition(mEditText.Text, expression.a, expression.b))
+                        if (BasicMathsFunctions.VerifyAddition(mEditText.Text,expression))
                         {
-                            //mTextView.TextColors = Android.Graphics.Color.Green;
                             mTextView.Text = "Dobrze!";
                             mbutton.SetBackgroundColor(Android.Graphics.Color.ParseColor("#00aced")); //blue
                             mbutton.Enabled = true;
+                            flag = true;
                         }
-                        if(!BasicMathsFunctions.VerifyAddition(mEditText.Text, expression.a, expression.b))
+                        if(!BasicMathsFunctions.VerifyAddition(mEditText.Text,expression))
                         {
                             mTextView.Text = "èle!";
                             mbutton.SetBackgroundColor(Android.Graphics.Color.ParseColor("#00aced")); //blue
                             mbutton.Enabled = true;
+                            flag = false;
                         }
                         i.Handled = true;
                     }
@@ -83,10 +83,11 @@ namespace Maths.Resources.Activities
             return ret;
         }
 
-        private int ShowCorectAnswer(int a, int b)
+        private void FalseAnswer(TextView mTextView, EditText mEditText)
         {
-            return a+b;
+            mTextView.Text = "Poprawna odpowiedü:";
+            mEditText.Text = Convert.ToString(expression.a + expression.b);
+            flag = true;
         }
-
     }
 }
