25b06b4c5aa6bf01576533c2b627766ec5d048e0
Grzegorz <grzesiek.p9804@gmail.com>
2017-04-08 19:55:25 +0200

    Added Settings/axml and Downgrade Xamarin to 4,2

--- Resources/Resource.Designer.cs
+++ Resources/Resource.Designer.cs
@@ -63,6 +63,9 @@ namespace Maths
 			// aapt resource value: 0x7f050002
 			public const int buttonAddiction2 = 2131034114;
 			
+			// aapt resource value: 0x7f050006
+			public const int buttonCombinedBasic2 = 2131034118;
+			
 			// aapt resource value: 0x7f050005
 			public const int buttonDivision2 = 2131034117;
 			
@@ -72,26 +75,35 @@ namespace Maths
 			// aapt resource value: 0x7f050003
 			public const int buttonSubtraction2 = 2131034115;
 			
-			// aapt resource value: 0x7f05000b
-			public const int button_View_BaseMaths_Next = 2131034123;
+			// aapt resource value: 0x7f05000f
+			public const int button_View_BaseMaths_Next = 2131034127;
 			
 			// aapt resource value: 0x7f050001
 			public const int buttonexit = 2131034113;
 			
-			// aapt resource value: 0x7f050008
-			public const int buttoninfo = 2131034120;
-			
-			// aapt resource value: 0x7f050006
-			public const int buttonlearn = 2131034118;
+			// aapt resource value: 0x7f050009
+			public const int buttoninfo = 2131034121;
 			
 			// aapt resource value: 0x7f050007
-			public const int buttonstatistics = 2131034119;
+			public const int buttonlearn = 2131034119;
+			
+			// aapt resource value: 0x7f050008
+			public const int buttonstatistics = 2131034120;
+			
+			// aapt resource value: 0x7f05000e
+			public const int edittext_View_BaseMaths = 2131034126;
 			
 			// aapt resource value: 0x7f05000a
-			public const int edittext_View_BaseMaths = 2131034122;
+			public const int relativeLayout1 = 2131034122;
 			
-			// aapt resource value: 0x7f050009
-			public const int textview_View_BaseMahs = 2131034121;
+			// aapt resource value: 0x7f05000c
+			public const int switch1 = 2131034124;
+			
+			// aapt resource value: 0x7f05000b
+			public const int textView1 = 2131034123;
+			
+			// aapt resource value: 0x7f05000d
+			public const int textview_View_BaseMahs = 2131034125;
 			
 			// aapt resource value: 0x7f050000
 			public const int textviewinfo = 2131034112;
@@ -119,7 +131,7 @@ namespace Maths
 			public const int Main = 2130903042;
 			
 			// aapt resource value: 0x7f030003
-			public const int Settings_BaseMaths = 2130903043;
+			public const int Settings_CombinedMaths = 2130903043;
 			
 			// aapt resource value: 0x7f030004
 			public const int Stats = 2130903044;
