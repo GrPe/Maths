436fd184944e1d7f30c807a9ab186e2be62794f6
Grzegorz <grzesiek.p9804@gmail.com>
2017-04-14 19:25:03 +0200

    Added RandDouble

--- Resources/Functionalities/RandValue.cs
+++ Resources/Functionalities/RandValue.cs
@@ -5,7 +5,6 @@ namespace Functionalities
     public static class RandValue
     {
         private static Random NValue = new Random();
-        private static int VRet;
         /// <summary>
         /// Return Random value from the specified range
         /// </summary>
@@ -14,8 +13,18 @@ namespace Functionalities
         /// <returns></returns>
         public static int Rand(int min, int max)
         {
-            VRet = NValue.Next(min, max);
-            return VRet;
+            return NValue.Next(min, max);
+        }
+
+        /// <summary>
+        /// Return Random value from the specified range
+        /// </summary>
+        /// <param name="min">minimal value</param>
+        /// <param name="max">maximal value</param>
+        /// <returns></returns>
+        public static double RandDouble(double min,double max)
+        {
+            return NValue.NextDouble() * (max - min) + min;
         }
     }
 }
